965a8ccb47df2b3953c55e2c5d8c6ae305b20459
Yanbo <bobihoeryan@gmail.com>
2024-05-28 00:46:32 +0700

    3D Holistic Walking, Tracking Head

--- Assets/_Scripts/Walker.cs
+++ Assets/_Scripts/Walker.cs
@@ -15,6 +15,9 @@ public class Walker : MonoBehaviour
     private Vector3 leftFootOffset;
     private Vector3 rightFootOffset;
 
+    private float leftLegLast = 0f;
+    private float rightLegLast = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,11 +28,35 @@ public class Walker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float leftLegForwardMovement = horizontalCurve.Evaluate(Time.time);
+        float rightLegForwardMovement = horizontalCurve.Evaluate(Time.time - 1f);
+
         leftFootTarget.localPosition = leftFootOffset +
-            this.transform.InverseTransformVector(leftFootTarget.forward) * horizontalCurve.Evaluate(Time.time) +
+            this.transform.InverseTransformVector(leftFootTarget.forward) * leftLegForwardMovement +
             this.transform.InverseTransformVector(leftFootTarget.up) * verticalCurve.Evaluate(Time.time + .5f);
         rightFootTarget.localPosition = rightFootOffset +
-            this.transform.InverseTransformVector(rightFootTarget.forward) * horizontalCurve.Evaluate(Time.time - 1) +
-            this.transform.InverseTransformVector(rightFootTarget.up) * verticalCurve.Evaluate(Time.time - .5f); ;
+            this.transform.InverseTransformVector(rightFootTarget.forward) * rightLegForwardMovement +
+            this.transform.InverseTransformVector(rightFootTarget.up) * verticalCurve.Evaluate(Time.time - .5f);
+
+        float leftLegDir = leftLegForwardMovement - leftLegLast;
+        float rightLegDir = rightLegForwardMovement - rightLegLast;
+
+        RaycastHit hit;
+        if(leftLegDir < 0f &&
+            Physics.Raycast(leftFootTarget.position + leftFootTarget.up, -leftFootTarget.up, out  hit, Mathf.Infinity))
+        {
+            leftFootTarget.position = hit.point;
+            this.transform.position += this.transform.forward * Mathf.Abs(leftLegDir);
+        }
+
+        if (rightLegDir < 0f &&
+            Physics.Raycast(rightFootTarget.position + rightFootTarget.up, -rightFootTarget.up, out hit, Mathf.Infinity))
+        {
+            rightFootTarget.position = hit.point;
+            this.transform.position += this.transform.forward * Mathf.Abs(rightLegDir);
+        }
+
+        leftLegLast = leftLegForwardMovement;
+        rightLegLast = rightLegForwardMovement; 
     }
 }
